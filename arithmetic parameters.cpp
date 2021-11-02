2db358961724bf270a3d457e30e6ddf528fc3101
deekshitha111 <93181791+deekshitha111@users.noreply.github.com>
2021-11-02 12:24:46 +0530

    Add files via upload

---
+++ arithmetic parameters.cpp
@@ -0,0 +1,19 @@
+ #include<stdio.h>
+ int main()
+ {
+ 	int a,b,add,sub,mul,div,per;
+ 	a=13;
+ 	b=45;
+ 	add=a+b;
+	 sub=a-b;
+	 mul=a*b;
+	 div=a/b;
+	 per=a%b;
+      printf("addition of %d and %d is %d \n",a,b,add);
+      printf("subtraction of %d and %d is %d\n"a,b,sub);
+      printf("muliplication of %d and %d is %d\n"a,b,mul);
+      printf("division od %d and %d is %d\n"a,b,div);
+      printf("percentage of %d and %d is %d\n"a,b,per);
+      return 0;
+}
+ 
