9da86ac652a31233e66a5006c4d846b00d84d578
deekshitha111 <93181791+deekshitha111@users.noreply.github.com>
2021-11-03 11:48:12 +0530

    Add files via upload

---
+++ time.c
@@ -0,0 +1,11 @@
+// converting minutes into hours
+#include<stdio.h>
+void main()
+{
+	int min,hour,minutes;
+	min=100;
+	hour=min/60;
+	minutes=min%60;
+	printf("%d hour(s) %d minute(s)",hour,minutes);
+	
+}
