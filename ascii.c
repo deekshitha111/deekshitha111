34050623606b28511b292b991d37240e1420312a
deekshitha111 <93181791+deekshitha111@users.noreply.github.com>
2021-11-09 12:29:39 +0530

    Add files via upload

---
+++ ascii.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+void main()
+{
+	int a;
+	printf("enter a number(65-90 or 97-122):");
+	scanf("%d",&a);
+	printf("corresponding ASCII character for %d is:",a,a);
+}
---
+++ char.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+void main()
+{
+	char h;
+	printf("enter a character:");
+	scanf("%c",&h);
+	printf("corresponding ASCII number for %c is :%d",h,h);
+}
