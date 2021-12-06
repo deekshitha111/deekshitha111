36e9b7107541b0de484058ec0c15c9785bb8c52b
deekshitha111 <93181791+deekshitha111@users.noreply.github.com>
2021-12-06 12:27:31 +0530

    Add files via upload

---
+++ 1121`.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int a,b;
+	printf(" enter two numbers\n");
+	scanf("%d%d",&a,&b);
+	if(a>b)
+	{
+		printf(" a is the largest number");
+	}
+	else
+	{
+		printf(" b is the largest number");
+	}
+}
---
+++ dee.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int a;
+	printf(" enter a number\n");
+	scanf("%d",&a);
+	if(a/2==0)
+	{
+		printf(" a is the even number");
+	}
+	else
+	{
+		printf("  a is the odd number");
+	}
+}
